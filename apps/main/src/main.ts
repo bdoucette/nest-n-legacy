34272ef7336a4f5066eedd3d3a8ee1b522dfd621
Brett Doucette <brettdoucette91@gmail.com>
2023-11-25 22:09:39 +0200

    no more any here

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -1,17 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { LegacyService } from './legacy/legacy.service';
 import { MainModule } from './main.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(MainModule);
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create<NestExpressApplication>(MainModule);
 
   await app.listen(3000);
 
-  const server = app.getHttpServer();
-  const router = server._events.request._router;
+  const server = app.getHttpAdapter().getInstance();
   const legacyService = app.get(LegacyService);
 
-  legacyService.registerNestJsRoutes(router);
+  legacyService.registerNestJsRoutes(server._router);
 }
 
 bootstrap();
